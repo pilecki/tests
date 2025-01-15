b94923fcc2f0545886f5e917c97b5204c43a35e5
pilecki <46523870+pilecki@users.noreply.github.com>
2025-01-15 20:56:31 +0900

    Update settings.py

--- ksiegarnia/settings.py
+++ ksiegarnia/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['https://tests-ps7x.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['tests-ps7x.onrender.com', 'localhost']
 
 
 # Application definition
