b832979889fb5d7f9e2d9eca2b3327a202bbf45a
pilecki <46523870+pilecki@users.noreply.github.com>
2025-01-16 12:21:01 +0900

    Update settings.py

--- ksiegarnia/settings.py
+++ ksiegarnia/settings.py
@@ -117,7 +117,7 @@ WSGI_APPLICATION = 'ksiegarnia.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))                                       )
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
     # Dodanie wymuszenia UTC dla PostgreSQL
     DATABASES['default']['OPTIONS'] = {
